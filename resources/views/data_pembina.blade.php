19ca74e9f4d905e117838f6d8b532fd4023ceea4
adymutalib <2110131110003@mhs.ulm.ac.id>
2023-05-09 11:46:23 +0700

    update

---
+++ resources/views/data_pembina.blade.php
@@ -0,0 +1,29 @@
+@extends('main')
+@section('container')
+<div class="card mt-5">
+    {{-- <img src="..." class="card-img-top" alt="..."> --}}
+    <div class="card-body">
+      <h5 class="card-title text-center">Data Pembina-1</h5>
+      <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, vero. Vero inventore provident, deleniti optio quam, illo ipsa numquam, fugit ad qui corrupti. Necessitatibus qui velit voluptate, excepturi, tenetur harum explicabo in dolores distinctio iure cum laudantium, beatae quos corporis laborum autem voluptates et neque! Odio repellendus possimus, quisquam officia minima laborum suscipit quia tenetur? Sed perspiciatis, expedita nobis laboriosam asperiores sint necessitatibus quod quis quibusdam totam ex. Facere sunt ullam excepturi molestiae, veniam eius repellat possimus amet soluta. Quae nihil dolores odio quis minima ea temporibus mollitia dicta, blanditiis atque quos cupiditate. Aspernatur pariatur temporibus ex, esse enim hic!</p>
+      {{-- <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p> --}}
+    </div>
+</div>
+
+<div class="card mt-5">
+    {{-- <img src="..." class="card-img-top" alt="..."> --}}
+    <div class="card-body">
+      <h5 class="card-title text-center">Data Pembina-2</h5>
+      <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, vero. Vero inventore provident, deleniti optio quam, illo ipsa numquam, fugit ad qui corrupti. Necessitatibus qui velit voluptate, excepturi, tenetur harum explicabo in dolores distinctio iure cum laudantium, beatae quos corporis laborum autem voluptates et neque! Odio repellendus possimus, quisquam officia minima laborum suscipit quia tenetur? Sed perspiciatis, expedita nobis laboriosam asperiores sint necessitatibus quod quis quibusdam totam ex. Facere sunt ullam excepturi molestiae, veniam eius repellat possimus amet soluta. Quae nihil dolores odio quis minima ea temporibus mollitia dicta, blanditiis atque quos cupiditate. Aspernatur pariatur temporibus ex, esse enim hic!</p>
+      {{-- <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p> --}}
+    </div>
+</div>
+
+<div class="card mt-5">
+    {{-- <img src="..." class="card-img-top" alt="..."> --}}
+    <div class="card-body">
+      <h5 class="card-title text-center">Data Pembina-3</h5>
+      <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, vero. Vero inventore provident, deleniti optio quam, illo ipsa numquam, fugit ad qui corrupti. Necessitatibus qui velit voluptate, excepturi, tenetur harum explicabo in dolores distinctio iure cum laudantium, beatae quos corporis laborum autem voluptates et neque! Odio repellendus possimus, quisquam officia minima laborum suscipit quia tenetur? Sed perspiciatis, expedita nobis laboriosam asperiores sint necessitatibus quod quis quibusdam totam ex. Facere sunt ullam excepturi molestiae, veniam eius repellat possimus amet soluta. Quae nihil dolores odio quis minima ea temporibus mollitia dicta, blanditiis atque quos cupiditate. Aspernatur pariatur temporibus ex, esse enim hic!</p>
+      {{-- <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p> --}}
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\orderController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('sidebare');
+// });
+
+//sidebare
+Route::get('/', [orderController::class, 'index']);
+//order
+Route::get('/order', function () {
+    return view('orders');
 });
+//navbar
+Route::get('/nav', function () {
+    return view('navbar');
+});
+//home
+Route::get('/home', function () {
+    return view('home');
+});
+//ekstra pembina
+Route::get('/pembina', function () {
+    return view('data_pembina');
+});
+//ekstra prestasi
+Route::get('/prestasi', function () {
+    return view('data_prestasi');
+});
+
+
